aec632f51b648b4d4e79228952f7866c0e0667ec
Jeff Ruoss <jeffreyruoss@gmail.com>
2016-02-20 18:06:01 +0000

    Constrain game to fixed size for now for faster/easier development.

--- js/stateManager.js
+++ js/stateManager.js
@@ -1,5 +1,9 @@
-var w = window.innerWidth * window.devicePixelRatio,
-    h = window.innerHeight * window.devicePixelRatio;
+// var w = window.innerWidth * window.devicePixelRatio,
+//     h = window.innerHeight * window.devicePixelRatio;
+    
+// setting fixed size right now for faster/easier development
+var w = 1200,
+    h = 600;
 
 var game = new Phaser.Game(w, h, Phaser.AUTO, 'gameContainer');
 
