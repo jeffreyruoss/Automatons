545ff34f82d11158a85d1b0cd219aa9b3e36ed6e
Jeff Ruoss <jeffreyruoss@gmail.com>
2016-02-21 20:31:58 -0700

    Modify the slash affect so it can be called on allies or enemies.

--- js/effects.js
+++ js/effects.js
@@ -1,7 +1,7 @@
 game.effects = {}
 
-game.effects.slash = function(enemy) {
-	var slash = game.add.sprite(enemy.x, enemy.y, 'Slash');
+game.effects.slash = function(character) {
+	var slash = game.add.sprite(character.x, character.y, 'Slash');
 	slash.animations.add('Slashing', [0, 1, 2, 3, 4]);
 	slash.animations.play('Slashing', 30, false);
 	slash.animations.currentAnim.onComplete.add(function() {
